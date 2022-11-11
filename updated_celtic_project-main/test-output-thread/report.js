fea2c0d8a299da69a4f0bdd8c2ea2945089ca792
madhusudhan <madhusudhan@upstreamgs.com>
2022-11-11 19:49:30 +0530

    first commit

---
+++ updated_celtic_project-main/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"19181ef4-67a4-4fa5-8195-23a4dd6bf83a","feature":"Renew Fleet feature","scenario":"IRP Renew fleet","start":1668160936718,"group":1,"content":"","tags":"","end":1668161706873,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
